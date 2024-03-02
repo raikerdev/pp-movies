b8f02f3f2276332f1957914ad76dedbe0eb108cd
Ricardo Ordoñez <ricardoaom.dev@gmail.com>
2024-03-01 19:46:14 -0500

    Configure Retrofit and test it with a first request

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,9 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.bundles.core)
+    implementation(libs.bundles.retrofit)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/raikerdev/petproject/movies/MainActivity.kt
+++ app/src/main/java/com/raikerdev/petproject/movies/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        lifecycleScope.launch {
+            val movies = RemoteConnection.service.listPopularMovies(getString(R.string.api_key))
+            toast(movies.results.size.toString())
         }
     }
 }
