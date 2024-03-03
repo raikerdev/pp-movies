76c64c65b28504cf4655fc856656f10bbc3b408c
Ricardo Ordoñez <ricardoaom.dev@gmail.com>
2024-03-02 21:20:36 -0500

    Use ListAdapter instead of basic one for movies

--- app/src/main/java/com/raikerdev/petproject/movies/ui/MoviesAdapter.kt
+++ app/src/main/java/com/raikerdev/petproject/movies/ui/MoviesAdapter.kt
@@ -3,28 +3,23 @@ package com.raikerdev.petproject.movies.ui
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.raikerdev.petproject.movies.R
 import com.raikerdev.petproject.movies.databinding.ViewMovieBinding
 import com.raikerdev.petproject.movies.model.Movie
 import kotlin.properties.Delegates
 
-class MoviesAdapter(private val listener: (Movie) -> Unit) : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
-
-    var movies: List<Movie> by basicDiffUtil(
-        emptyList(),
-        areItemsTheSame = { old, new -> old.id == new.id}
-    )
+class MoviesAdapter(private val listener: (Movie) -> Unit) :
+    ListAdapter<Movie, MoviesAdapter.ViewHolder>(basicDiffUtil { old, new -> old.id == new.id }) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = parent.inflate(R.layout.view_movie, false)
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int = movies.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val movie = movies[position]
+        val movie = getItem(position)
         holder.bind(movie)
         holder.itemView.setOnClickListener { listener(movie) }
     }
