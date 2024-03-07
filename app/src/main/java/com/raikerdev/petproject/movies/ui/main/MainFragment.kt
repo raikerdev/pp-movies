3ff2db4d4c497fb1bebce65a7b4b31ed09a31bc1
Ricardo Ordoñez <ricardoaom.dev@gmail.com>
2024-03-06 22:40:39 -0500

    Use StateHolder for MainFragment

--- app/src/main/java/com/raikerdev/petproject/movies/ui/main/MainFragment.kt
+++ app/src/main/java/com/raikerdev/petproject/movies/ui/main/MainFragment.kt
@@ -1,20 +1,14 @@
 package com.raikerdev.petproject.movies.ui.main
 
-import android.Manifest
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import com.raikerdev.petproject.movies.R
 import com.raikerdev.petproject.movies.databinding.FragmentMainBinding
-import com.raikerdev.petproject.movies.model.Movie
 import com.raikerdev.petproject.movies.model.MoviesRepository
-import com.raikerdev.petproject.movies.ui.common.PermissionRequester
 import com.raikerdev.petproject.movies.ui.common.launchAndCollect
-import kotlinx.coroutines.launch
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
@@ -22,39 +16,28 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         MainViewModelFactory(MoviesRepository(requireActivity().application))
     }
 
-    private val adapter = MoviesAdapter { viewModel.onMovieClicked(it) }
-
-    private val coarsePermissionRequester = PermissionRequester(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+    private val adapter = MoviesAdapter { mainState.onMovieClicked(it) }
+    private lateinit var mainState: MainState
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mainState = buildMainState()
+
         val binding = FragmentMainBinding.bind(view).apply {
             recycler.adapter = adapter
         }
 
         viewLifecycleOwner.launchAndCollect(viewModel.state) { binding.updateUI(it) }
+
+        mainState.requestLocationPermission { viewModel.onUiReady() }
+
     }
 
     private fun FragmentMainBinding.updateUI(state: MainViewModel.UiState) {
         progress.isVisible = state.loading
         state.movies?.let(adapter::submitList)
-        state.navigateTo?.let(::navigateTo)
-        if(state.requestLocationPermission) {
-            requestLocationPermission()
-        }
     }
 
-    private fun navigateTo(movie: Movie) {
-        val navAction = MainFragmentDirections.actionMainToDetail(movie)
-        findNavController().navigate(navAction)
-        viewModel.onNavigationDone()
-    }
-
-    private fun requestLocationPermission() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            coarsePermissionRequester.request()
-            viewModel.onLocationPermissionChecked()
-        }
-    }
 
 }
---
+++ app/src/main/java/com/raikerdev/petproject/movies/ui/main/MainState.kt
@@ -0,0 +1,37 @@
+package com.raikerdev.petproject.movies.ui.main
+
+import android.Manifest
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.raikerdev.petproject.movies.model.Movie
+import com.raikerdev.petproject.movies.ui.common.PermissionRequester
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+fun Fragment.buildMainState(
+    scope: CoroutineScope = viewLifecycleOwner.lifecycleScope,
+    navController: NavController = findNavController(),
+    locationPermissionRequester: PermissionRequester = PermissionRequester(this, Manifest.permission.ACCESS_COARSE_LOCATION),
+) = MainState(scope, navController, locationPermissionRequester)
+
+class MainState(
+    private val scope: CoroutineScope,
+    private val navController: NavController,
+    private val locationPermissionRequester: PermissionRequester,
+) {
+
+    fun onMovieClicked(movie: Movie) {
+        val navAction = MainFragmentDirections.actionMainToDetail(movie)
+        navController.navigate(navAction)
+    }
+
+    fun requestLocationPermission(afterRequest: (Boolean) -> Unit) {
+        scope.launch {
+            val result = locationPermissionRequester.request()
+            afterRequest(result)
+        }
+    }
+
+}
